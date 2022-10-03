ab2ffa97314d20cb7a9c6a2fc9151c058108e3d6
sarbanandabhikkhu <sarbanandabhikkhu@gmail.com>
2022-10-03 13:31:00 +0600

    sample layout markup complete

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,9 +1,37 @@
 import React, { useState, useEffect, useRef } from "react";
-import Chevron from "../../icons/angle-down.svg";
+import chevron from "../../icons/angle-down.svg";
+import folder from "../../icons/folder.svg";
+import document from "../../icons/document.svg";
+
+function AccordionLink({ toggle, title }) {
+  const handleClick = () => {
+    alert("Hey, content link is clicked!");
+  };
+
+  const titleDepth = title.length - 1;
+
+  return (
+    <a
+      className="accordion-link"
+      href="#"
+      onClick={handleClick}
+      aria-hidden={toggle ? "true" : "false"}
+    >
+      <img src={document} />
+      {title[titleDepth].trim().length > 20
+        ? title[titleDepth].trim().substring(0, 20) + "..."
+        : title[titleDepth].trim()}
+    </a>
+  );
+}
 
 function Accordion() {
   const [toggle, setToggle] = useState(false);
   const [heightEl, setHeightEl] = useState();
+  const [parajika, setParajika] = useState(false);
+  const URL =
+    "https://sarbanandabhikkhu.github.io/DhammaChakka/data/pārājika.json";
+  // "http://localhost:8080/data/pārājika.json";
 
   const refHeight = useRef();
 
@@ -16,25 +44,39 @@ function Accordion() {
     setToggle(!toggle);
   };
 
-  console.log(toggle);
+  useEffect(() => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setParajika(data);
+        console.log(data);
+      });
+  }, []);
 
   return (
     <div className="Accordion">
-      <button onClick={toggleState} className="AccordionBtn">
-        <img className={toggle && "active"} src={Chevron} />
-        <span>Lorem ipsum</span>
+      <button
+        onClick={toggleState}
+        className={
+          toggle && toggle ? "accordion-btn --active" : "accordion-btn"
+        }
+      >
+        <img
+          className={toggle && toggle ? "indicates active" : "indicates"}
+          src={chevron}
+        />
+        <img src={folder} />
+        <span>parajika</span>
       </button>
       <div
         ref={refHeight}
-        className={toggle ? "AccordionToggle animated" : "AccordionToggle"}
+        className={toggle ? "accordion-panel animated" : "accordion-panel"}
         style={{ height: toggle ? `${heightEl}` : "0px" }}
       >
-        <p aria-hidden={toggle ? "true" : "false"}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore,
-          suscipit quae maiores sunt ducimus est dolorem perspiciatis earum
-          corporis unde, dicta quibusdam aut placeat dignissimos distinctio vel
-          quo eligendi ipsam.
-        </p>
+        {parajika &&
+          parajika.map((data) => (
+            <AccordionLink toggle={toggle} title={data.title.split(",")} />
+          ))}
       </div>
     </div>
   );
--- src/components/asidebar/index.js
+++ src/components/asidebar/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Accordion from "../accordion";
+import items from "../../icons/menu-dots.svg";
 
-const AsideBar = (prps) => {
+const AsideBar = () => {
+  const handleClick = () => {
+    alert("Hey, Explore action button is clicked!");
+  };
   return (
     <div className="AsideBar">
       <div className="TopBar">
         <span>Explore</span>
+        <img src={items} onClick={handleClick} />
       </div>
       <Accordion />
     </div>
